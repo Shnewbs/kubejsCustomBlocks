5d03030e43a0d4660d8733f44860dedce4464f42
Shnewbs <90636566+Shnewbs@users.noreply.github.com>
2023-12-07 02:42:02 -0800

    pathway corrections

--- startup_scripts/stone/pathways.js
+++ startup_scripts/stone/pathways.js
@@ -22,7 +22,7 @@ TFCStoneVarient.forEach(TFCStoneVarient => {
   TFCStoneTypes.forEach(TFCStoneTypes => {
     let displayMaterial = capfirst(`${TFCStoneTypes}`)    
 
-  event.create(`${TFCStoneVarient}.${PathStyles}.pathway.${TFCStoneTypes}`, 'cardinal') // Create a new block
+  event.create(`${TFCStoneVarient}.pathway.${PathStyles}.${TFCStoneTypes}`, 'cardinal') // Create a new block
 
         .displayName(`${TFCStoneOptionNames} ${displayMaterial} ${PathNames} Path`) // Set a custom name
         .soundType("stone") // Set a material (affects the sounds and some properties)
@@ -31,9 +31,6 @@ TFCStoneVarient.forEach(TFCStoneVarient => {
         .model(`kubejs:block/stone/${TFCStoneVarient}/pathway/${PathStyles}/${TFCStoneTypes}`)
         .defaultCutout()
         .waterlogged()
-        .box(8, 0, 9, 15, 1, 16)
-        .box(8, 0, 1, 15, 1, 8)
-        .box(0, 0, 9, 7, 1, 16)
-        .box(0, 0, 1, 7, 1, 8)
+        .box(0, 0, 0, 16, 0.8, 16)
 })})
 })})
